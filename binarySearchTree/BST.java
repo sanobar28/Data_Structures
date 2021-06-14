28e98dd6ef0c899ccff7429e35b45625c39aee74
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-14 23:17:21 +0530

    added: Method to find correct position to insert node

--- binarySearchTree/BST.java
+++ binarySearchTree/BST.java
@@ -49,6 +49,30 @@ public class BST{
 			}
 		}
 	}
+	
+	/**
+	 * UC-2 Traverse BST to get correct position of node
+	 * @param node
+	 */
+	 public void insertNodeAt(Node node) {  
+		  
+         //Check whether tree is empty  
+         if(root == null){  
+             System.out.println("Tree is empty");  
+             return;  
+          }  
+         else {  
+ 
+             if(node.left!= null)  
+                 insertNodeAt(node.left);  
+             System.out.print(node.data + " ");  
+             if(node.right!= null)  
+                 insertNodeAt(node.right);  
+ 
+         }  
+     }  
+	
+	
 
 	// minNode() will find out the minimum node
 	public Node minNode(Node root) {
@@ -65,6 +89,20 @@ public class BST{
 		tree.insert(56);
 		tree.insert(30);
 		tree.insert(70);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(11);
+		tree.insert(65);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(63);
+		tree.insert(67);
+		
+		System.out.println("Binary search tree after insertion:");  
+        //Displays the binary tree  
+        tree.insertNodeAt(tree.root);  
 		
 	}
 }
